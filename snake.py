14aeafd2be3454cd538c677501a60fd677700b01
Alexander Höreth <ahoereth@gmail.com>
2017-01-06 17:16:07 +0100

    Make vizualizations optional.

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import random as rand
 from matplotlib import animation, pyplot as plt
 import sys
+import threading
 
 
 dirs = {
@@ -19,23 +20,33 @@ BG = 0
 
 
 class Snake:
-    def __init__(self, figure, speed=50, listener=lambda: None):
+    def __init__(self, figure, speed=50, viz=True, listener=lambda: None):
         self.listener = listener
         self.fig = figure
         self.size = 32
-        self.fig.canvas.mpl_connect('key_press_event', self.input)
         self.start()
-        self.img = plt.imshow(self.board, cmap='gray_r', vmin=0, vmax=2)
-        self.anim = animation.FuncAnimation(self.fig, self.step,
-                                            interval=1100//speed, blit=True)
+        self.viz = viz
+        self.speed = speed
+        if viz:
+            self.fig.canvas.mpl_connect('key_press_event', self.keypress)
+            self.img = plt.imshow(self.board, cmap='gray_r', vmin=0, vmax=2)
+            self.anim = animation.FuncAnimation(self.fig, self.step,
+                                                interval=1100//speed,
+                                                blit=True)
+        else:
+            thread = threading.Timer(1/self.speed, self.step)
+            thread.start()
+
 
     def start(self):
+        self.steps = 0
         self.board = np.ones((self.size, self.size)) * BG
         self.board.flat[rand.randint(0, len(self.board) - 1)] = FOOD
         self.body = [rand.randint(0, len(self.board) - 1)]
         self.dir = list(dirs.values())[np.random.randint(0, 4)]
 
-    def step(self, i):
+    def step(self, **args):
+        self.steps += 1
         r, c = self.get_head(rc=True)
         if self.dir & 2:  # horizontal
             c = (c + (self.dir & 1) * 2 - 1) % self.size
@@ -52,9 +63,13 @@ class Snake:
             self.board.flat[self.body.pop(0)] = BG
         self.body.append(head)
         self.board.flat[head] = SNAKE
-        self.img.set_data(self.board)
         self.event('highscore')
-        return self.img,
+        if self.viz:
+            self.img.set_data(self.board)
+            return self.img,
+        else:
+            thread = threading.Timer(1/self.speed, self.step)
+            thread.start()
 
     def get_highscore(self):
         return len(self.body) - MINSNAKELEN
@@ -67,19 +82,21 @@ class Snake:
         food = np.flatnonzero(self.body == FOOD)[0]
         return np.unravel_index(food, self.board.shape) if rc else food
 
-    def input(self, event):
-        key = event.key.upper()
+    def set_direction(self, direction):
+        if direction in dirs.values() and direction ^ 1 != self.dir:
+            self.dir = direction
+
+    def keypress(self, event):
+        key = (event.key or '').upper()
         if key == 'ESCAPE':
             sys.exit()
         if key in dirs.keys():
-            direction = dirs[key]
-            if direction ^ 1 != self.dir:
-                self.dir = direction
+            self.set_direction(dirs[key])
 
     def event(self, name):
         self.listener(event=name, highscore=self.get_highscore(),
                       direction=self.dir, board=self.board,
-                      head=self.get_head())
+                      head=self.get_head(), steps=self.steps)
 
 
 if __name__ == '__main__':
