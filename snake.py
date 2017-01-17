3cb07e91b1c912724f92da98ed13690cdeefdc9c
Alexander Höreth <ahoereth@gmail.com>
2017-01-17 17:09:06 +0100

    Refactor snake game into 3 classes. props @AndreaSuckro.

--- snake.py
+++ snake.py
@@ -2,9 +2,8 @@ import numpy as np
 from numpy import random as rand
 import matplotlib
 matplotlib.use('tkAgg')
-from matplotlib import animation, pyplot as plt
+from matplotlib import pyplot as plt
 import sys
-import threading
 
 
 actions = {
@@ -15,107 +14,95 @@ actions = {
 }
 
 
-MINSNAKELEN = 5
 SNAKE = 2
 FOOD = 1
 BG = 0
 
 
 class Snake:
-    def __init__(
-        self, figure, speed=50, viz=True, listener=lambda: None,
-        restart=False,
-    ):
-        self.listener = listener
-        self.fig = figure
-        self.size = 8
-        self.start()
-        self.viz = viz
-        self.speed = speed
-        self.restart = restart
-        if viz:
-            self.fig.canvas.mpl_connect('key_press_event', self.keypress)
-            self.img = plt.imshow(self.board, cmap='gray_r', vmin=0, vmax=2)
-            self.anim = animation.FuncAnimation(self.fig, self.step,
-                                                interval=1100//speed,
-                                                blit=True)
-        else:
-            thread = threading.Timer(1/self.speed, self.step)
-            thread.start()
+    def __init__(self, size=8, onstep=lambda x: x):
+        self.onstep = onstep
+        self.size = size
+        self.minsnakelen = 5
 
-
-    def start(self):
         self.steps = 0
         self.board = np.ones((self.size, self.size)) * BG
         self.body = [rand.randint(0, len(self.board) - 1)]
-        food = rand.choice(np.flatnonzero(self.board == BG))
-        self.board.flat[food] = FOOD
+        self.board.flat[self.body[0]] = SNAKE
+        self.board.flat[rand.choice(np.flatnonzero(self.board == BG))] = FOOD
         self.dir = list(actions.values())[np.random.randint(0, 4)]
+        print(self.dir)
 
-    def step(self, *args):
-        self.steps += 1
-        r, c = self.get_head(rc=True)
-        if self.dir & 2:  # horizontal
+    def step(self, direction=None):
+        if direction in actions.values() and direction ^ 1 != self.dir:
+            self.dir = direction
+        else:
+            direction = self.dir
+
+        r, c = self.get_head()
+        if direction & 2:  # horizontal
             c = (c + (self.dir & 1) * 2 - 1) % self.size
         else:  # vertical
             r = (r + (self.dir & 1) * 2 - 1) % self.size
+
         head = np.ravel_multi_index((r, c), self.board.shape)
-        if self.board.flat[head] == SNAKE:
-            self.event('lost')
-            if self.restart:
-                self.start()
+        if self.board.flat[head] == SNAKE:  # died
+            return -1
+
         if self.board.flat[head] == FOOD:  # caught a special dot
             food = rand.choice(np.flatnonzero(self.board == BG))
             self.board.flat[food] = FOOD
-        elif len(self.body) > MINSNAKELEN:  # otherwise cut of tail
+        elif len(self.body) > self.minsnakelen:  # otherwise cut of tail
             self.board.flat[self.body.pop(0)] = BG
+
+        self.steps += 1
         self.body.append(head)
         self.board.flat[head] = SNAKE
-        self.event('highscore')
-        if self.viz:
-            self.img.set_data(self.board)
-            return self.img,
-        else:
-            thread = threading.Timer(1/self.speed, self.step)
-            thread.start()
+        self.onstep(board=self.board)
+        return self.steps
 
     def get_highscore(self):
-        return len(self.body) - MINSNAKELEN
+        return len(self.body) - self.minsnakelen
 
-    def get_head(self, rc=False):
+    def get_head(self, rc=True):
         head = self.body[len(self.body) - 1]
         return np.unravel_index(head, self.board.shape) if rc else head
 
-    def get_food(self, rc=False):
+    def get_food(self, rc=True):
         food = np.flatnonzero(self.body == FOOD)[0]
         return np.unravel_index(food, self.board.shape) if rc else food
 
-    def act(self, direction):
-        if direction in actions.values() and direction ^ 1 != self.dir:
-            self.dir = direction
+
+class SnakeUI:
+    def __init__(self):
+        self.fig = plt.figure()
+        plt.axis('off')
+        self.img = None
+
+    def redraw(self, board):
+        if self.img is None:
+            self.img = plt.imshow(board, cmap='gray_r', vmin=0, vmax=2)
+        self.img.set_data(board)
+        plt.draw()
+
+
+class SnakeKeyboard:
+    def __init__(self, snake, ui):
+        self.snake = snake
+        ui.fig.canvas.mpl_connect('key_press_event', self.keypress)
+        plt.show()
 
     def keypress(self, event):
         key = (event.key or '').upper()
         if key == 'ESCAPE':
             sys.exit()
         if key in actions.keys():
-            self.act(actions[key])
-
-    def event(self, name):
-        self.listener(event=name, highscore=self.get_highscore(),
-                      direction=self.dir, board=self.board,
-                      head=self.get_head(), steps=self.steps)
+            if self.snake.step(actions[key]) == -1:
+                sys.exit()
 
 
 if __name__ == '__main__':
-    def listen(event, highscore, steps, **args):
-        sys.stdout.write('\r%s | %s | %s' % (event, highscore, steps))
-        if event == 'lost':
-            sys.stdout.write('\n')
-        sys.stdout.flush()
-
-    fig = plt.figure()
-    size = 256
-    plt.axis('off')
-    snake = Snake(fig, speed=10, listener=listen)
-    plt.show()
+    ui = SnakeUI()
+    snake = Snake(size=4, onstep=ui.redraw)
+    ui.redraw(snake.board)
+    SnakeKeyboard(snake, ui)
