0f0cd65dde69eaca0325791159a7845ed0008c00
Andrea <a.suckro@gmail.com>
2017-01-23 17:16:14 +0100

    it is alive!

--- evolve_snake.py
+++ evolve_snake.py
@@ -1,3 +1,4 @@
+import itertools
 from multiprocessing import Pool
 
 import numpy as np
@@ -5,12 +6,14 @@ import tensorflow as tf
 
 from snake import Snake
 
+
 BOARD_SIZE = 4
 DIRECTIONS = 4
 
 MAX_GAME_STEPS = 100
-MAX_INDIVIDUALS = 5
-MAX_GENERATIONS = 1  # 00
+MAX_INDIVIDUALS = 22
+WINNER_RATIO = .5
+MAX_GENERATIONS = 500  # 00
 
 NUM_POOLS = 4
 
@@ -23,14 +26,13 @@ class EvolveSnake:
         self.snake = snake
         self.weights = weights
         if self.weights is None:
-            size = self.snake.board.size
+            size = self.snake.board.size + 1  # for bias term
             self.weights = np.random.random((size, DIRECTIONS))
 
     def init_network(self):
-        board = tf.placeholder(PRECISION, [None, self.snake.board.size])
+        board = tf.placeholder(PRECISION, [None, self.weights.size/DIRECTIONS])
         w1 = tf.Variable(self.weights, name='weights')
-        b = tf.Variable(tf.ones([DIRECTIONS], dtype=PRECISION))
-        output_layer = tf.nn.relu_layer(board, w1, b, name='output')
+        output_layer = tf.nn.relu(tf.matmul(board, w1), name='output')
         action = tf.argmax(tf.nn.softmax(output_layer), 1)
         return board, action
 
@@ -40,7 +42,7 @@ class EvolveSnake:
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             for i in range(MAX_GAME_STEPS):
-                act = sess.run(action, {board: [self.snake.board.flatten()]})
+                act = sess.run(action, {board: [np.append(self.snake.board.flatten(),1)]})
                 if self.snake.step(act) == -1:
                     break
 
@@ -53,8 +55,35 @@ def play_snake(snake):
 
 
 class SnakeTrainer:
-    def generate_snakes(self, number):
-        return [EvolveSnake(Snake(BOARD_SIZE)) for i in range(number)]
+    def generate_snakes(self, number, weights = None):
+        if weights is None:
+            snakes = [EvolveSnake(Snake(BOARD_SIZE)) for i in range(number)]
+        else:
+            snakes = [EvolveSnake(Snake(BOARD_SIZE),weight) for weight in weights]
+        return snakes
+
+
+    def get_offsprings(self, parents):
+        babies = []
+        for a, b in itertools.combinations(parents, 2):
+            baby = (a+b)/2
+            babies.append(baby)
+        return babies
+
+    def build_next_generation(self, results):
+        # sort for highscore per step
+        keep = int(np.ceil(MAX_INDIVIDUALS*WINNER_RATIO))
+        ranked_individuals = sorted(results, key=lambda x: x[1]/x[2], reverse=True)
+
+        best_individuals = [i[0] for i in ranked_individuals[0:keep]]
+        offsprings = self.get_offsprings(best_individuals)
+        random_new = self.generate_snakes(MAX_INDIVIDUALS-keep-len(offsprings))
+
+        new_gen = self.generate_snakes(0, best_individuals + offsprings) + random_new
+        return new_gen
+
+
+
 
 
 if __name__ == '__main__':
@@ -64,6 +93,7 @@ if __name__ == '__main__':
     for i in range(MAX_GENERATIONS):
         with Pool(NUM_POOLS) as p:
             results = p.map(play_snake, tensor_snakes)
+        tensor_snakes = trainer.build_next_generation(results)
 
     for w, h, s in results:
         print(h, s)
