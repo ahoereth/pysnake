8d1658fa1af870b709360d987e8e5fc83826c7e4
Andrea <a.suckro@gmail.com>
2017-02-14 17:09:20 +0100

    implementing basic crossover algorithm for the evolve snake and adding docs

--- evolve_snake.py
+++ evolve_snake.py
@@ -1,4 +1,5 @@
 import itertools
+import collections
 from multiprocessing import Pool
 
 import numpy as np
@@ -6,18 +7,19 @@ import tensorflow as tf
 
 from snake import Snake
 
-
 BOARD_SIZE = 4
 DIRECTIONS = 4
 
 MAX_GAME_STEPS = 100
 MAX_INDIVIDUALS = 100
-WINNER_RATIO = .5
-MAX_GENERATIONS = 500  # 00
+MAX_GENERATIONS = 20  # 00
 
+HIDDEN = 8
+
+MUTATIONRATE = 0.001
+KEEP = 10  # The permutation of this is also kept in the 'children'
 
 NUM_POOLS = 4
-
 PRECISION = tf.float64
 
 
@@ -25,7 +27,7 @@ class EvolveSnake:
     def __init__(self, snake, weights=None):
         """Initializes a TensorFlow graph to play snake."""
         self.snake = snake
-        self.layers = [self.snake.board.size + 1, 4, DIRECTIONS]
+        self.layers = [self.snake.board.size + 1, HIDDEN, DIRECTIONS]
         self.weights = weights
         if self.weights is None:
             self.weights = [np.random.random((size, self.layers[i+1]))
@@ -60,7 +62,15 @@ def play_snake(snake):
 
 
 class SnakeTrainer:
+
     def generate_snakes(self, number_or_weights):
+        """
+        This function generates Snakes with random or predefined weights.
+
+        :param number_or_weights: if an integer argument is given, this number of new Snakes is generated
+                                  if a list of weights is given, those are used to initialize the network
+        :return: the generated Snakes
+        """
         try:
             snakes = [EvolveSnake(Snake(BOARD_SIZE), weights)
                       for weights in number_or_weights]
@@ -69,23 +79,68 @@ class SnakeTrainer:
                       for i in range(number_or_weights)]
         return snakes
 
+    def flatten(l):
+        """
+        Flattens a list containing several sublists into a flat list.
+
+        :return: a flattened representation of the given list
+        """
+        for el in l:
+            if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes)):
+                yield from SnakeTrainer.flatten(el)
+            else:
+                yield el
+
     def get_offsprings(self, parents):
+        """
+        Generates the offspring from two controller Networks.
+
+        :param parents: The parents which weight values are being used to generate the offspring
+        :return: the generated offspring networks
+        """
         babies = []
         for mum, dad in itertools.combinations(parents, 2):
-            baby = [(a + b) / 2 for a, b in zip(mum, dad)]
+
+            flat_mum = list(SnakeTrainer.flatten(mum))
+            flat_dad = list(SnakeTrainer.flatten(dad))
+
+            baby_flat = []
+            for i in range(0, len(flat_mum)):
+                # Step 1: Random Crossover
+                # choose with equal probability the weight for the child from mum and dad
+                baby_flat.append(flat_mum[i] if np.random.random_sample() < 0.5 else flat_dad[i])
+                # Step 2: Mutation
+                if np.random.random_sample() < MUTATIONRATE:
+                    baby_flat[i] = np.random.random_sample()
+
+            # reshaping back into layered form
+            start_w = 0
+            baby = []
+            for i in range(len(mum)):
+                lx, ly = mum[i].shape
+                layer = np.array(baby_flat)[start_w: start_w + lx * ly].reshape(mum[i].shape)
+                start_w = start_w + lx * ly
+                baby.append(layer)
+
             babies.append(baby)
         return babies
 
     def build_next_generation(self, results):
-        # sort for highscore per step
-        keep = int(np.ceil(MAX_INDIVIDUALS * WINNER_RATIO))
-        ranked_individuals = sorted(results, key=lambda x: x[1] / x[2],
-                                    reverse=True)
+        """
+        Builds the next generation from the previous training results.
 
-        
-        best_individuals = [i[0] for i in ranked_individuals[0:keep]]
+        :param results: the individuals and performances from the prior run
+        :return: a new generation to be used
+        """
+        # sort for highscore per step
+        ranked_individuals = sorted(results, key=lambda x: x[1] / x[2], reverse=True)
+
+        best_individuals = [i[0] for i in ranked_individuals[0:KEEP]]
         offsprings = self.get_offsprings(best_individuals)
-        new = self.generate_snakes(MAX_INDIVIDUALS - keep - len(offsprings))
+        spawning = MAX_INDIVIDUALS - KEEP - len(offsprings)
+
+        if(spawning > 0):
+            new = self.generate_snakes(spawning)
 
         new_gen = self.generate_snakes(best_individuals + offsprings) + new
         return new_gen
