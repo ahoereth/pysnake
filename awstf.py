be00bd40157ebcad4456df7052d8866a21f672eb
Alexander Höreth <ahoereth@gmail.com>
2017-02-17 14:46:28 +0100

    Cleanup awstf.

--- awstf.py
+++ awstf.py
@@ -14,6 +14,19 @@ AMIS = {
 }
 
 
+COMMAND = '''\
+docker-machine create {name} \\
+    --driver amazonec2 \\
+    --amazonec2-region {region} \\
+    --amazonec2-zone {zone} \\
+    --amazonec2-ami {ami} \\
+    --amazonec2-instance-type {instance_type} \\
+    --amazonec2-security-group {security_group} \\
+    --amazonec2-request-spot-instance \\
+    --amazonec2-spot-price {price_max:.4f}\
+'''
+
+
 useast1 = boto3.client('ec2', region_name='us-east-1')
 euwest1 = boto3.client('ec2', region_name='eu-west-1')
 clients = [useast1, euwest1]
@@ -39,41 +52,31 @@ def get_avg_price(instance_type, hours=5):
     return sorted(prices, key=lambda t: t[1])
 
 
-command = '''\
-docker-machine create \\
-    --driver amazonec2 \\
-    --amazonec2-region {region} \\
-    --amazonec2-zone {zone} \\
-    --amazonec2-ami {ami} \\
-    --amazonec2-instance-type {instance_type} \\
-    --amazonec2-security-group {security_group} \\
-    --amazonec2-request-spot-instance \\
-    --amazonec2-spot-price {price_max:.4f} \\
-    {name}\
-'''
-
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-t', '--instance-type', default='p2.xlarge')
-    parser.add_argument('-sg', '--security-group', default='docker-machine')
-    parser.add_argument('--max-price-on-top', default=.1, type=float)
-    parser.add_argument('--hours', default=5, type=float)
-    args = parser.parse_args()
-
+def main(machine_name, instance_type, security_group, max_price_overhead, hours):
     zone, price = get_avg_price(args.instance_type, hours=args.hours)[0]
-
     print('# Instances of type {instance_type} are cheapest in region {zone} '
           'with an average price of US${price:.4f} over the last {hours} hours.'
           .format(instance_type=args.instance_type, zone=zone, price=price,
                   hours=args.hours))
     print('# Issue the following command to launch a spot instance '
           'or call this script with eval $(SCRIPT). \n')
-    print(command.format(
+    print(COMMAND.format(
+        name=machine_name,
         region=zone[:-1],
         zone=zone[-1],
-        instance_type=args.instance_type,
-        security_group=args.security_group,
-        price_max=price + args.max_price_on_top,
-        ami=AMIS[(args.instance_type, zone[:-1])],
-        name='aws42',
+        instance_type=instance_type,
+        security_group=security_group,
+        price_max=price + max_price_overhead,
+        ami=AMIS[(instance_type, zone[:-1])],
     ))
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('machine_name')
+    parser.add_argument('-t', '--instance-type', default='p2.xlarge')
+    parser.add_argument('-sg', '--security-group', default='docker-machine')
+    parser.add_argument('--max-price-overhead', default=.1, type=float)
+    parser.add_argument('--hours', default=5, type=float)
+    args = parser.parse_args()
+    main(**vars(args))
